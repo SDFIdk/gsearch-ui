081eb02719163f10ceec3e0a27056bc938867cfe
Frank Thomsen <frtho@sdfi.dk>
2022-12-01 20:05:19 +0100

    update automation scripts

--- esbuild.js
+++ esbuild.js
@@ -3,19 +3,44 @@ const entry_points = {
   style: 'src/index.css'
 }
 
-// Development mode watches for file changes and rebuilds
-require('esbuild').serve({
-  servedir: 'public',
-}, {
-  entryPoints: entry_points,
-  loader: {
-    '.ttf': 'file'
-  },
-  outdir: 'public',
-  bundle: true,
-  format: 'esm'
-}).then(server => {
-  console.log(server)
-  // Call "stop" on the web server to stop serving
-  //server.stop()
-})
+
+if (process.env.NODE_ENV === 'development') {
+
+  // Development mode watches for file changes and rebuilds
+  require('esbuild').serve({
+    servedir: 'public',
+  }, {
+    entryPoints: entry_points,
+    loader: {
+      '.ttf': 'file'
+    },
+    outdir: 'public',
+    bundle: true,
+    format: 'esm'
+  }).then(server => {
+    console.log('Running development server')
+    console.log(server)
+    // Call "stop" on the web server to stop serving
+    //server.stop()
+  })
+
+} else {
+
+  // Production build
+  require('esbuild').build({
+    entryPoints: entry_points,
+    loader: {
+      '.ttf': 'file'
+    },
+    outdir: 'public',
+    bundle: true,
+    format: 'esm',
+    minify: true,
+    sourcemap: true
+  })
+  .then((response) => {
+    console.log('Build finished 👍')
+  })
+  .catch(() => process.exit(1))
+
+}
