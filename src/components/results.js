f39cf7b64b65a8ce18bce9eb610ede50226beb0b
Gebuz <johanlundn@gmail.com>
2022-12-12 13:16:22 +0100

    Update results.js

--- src/components/results.js
+++ src/components/results.js
@@ -60,21 +60,23 @@ export class GSearchResults extends HTMLElement {
         roads.push(el)
       }
     }) */
-    data.forEach((el) => {
-      /* Commented out for testing purposes to show the clean data.
-      if (el.type === 'husnummer' || el.type === 'adresse') {
-        if (roads.length > 1 && roads.find((road) => {
-          return road.vejnavn === el.vejnavn && road.postnummer === el.postnummer
-        })) {
-          return
-        }
-      } */
-      // if the result boxes end up with enough significant differences to justify a class for each, use this:
-      // const listItem = document.createElement('g-search-result-box-' + el.type)
-      const listItem = document.createElement('g-search-result-box')
-      listItem.result = el
-      list.append(listItem)
-    })
+    else {
+      data.forEach((el) => {
+        /* Commented out for testing purposes to show the clean data.
+        if (el.type === 'husnummer' || el.type === 'adresse') {
+          if (roads.length > 1 && roads.find((road) => {
+            return road.vejnavn === el.vejnavn && road.postnummer === el.postnummer
+          })) {
+            return
+          }
+        } */
+        // if the result boxes end up with enough significant differences to justify a class for each, use this:
+        // const listItem = document.createElement('g-search-result-box-' + el.type)
+        const listItem = document.createElement('g-search-result-box')
+        listItem.result = el
+        list.append(listItem)
+      })
+    }
     this.shadowRoot.innerHTML = this.template
     this.shadowRoot.append(list)
   }
