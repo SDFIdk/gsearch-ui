0fd84e6ea8f76f96025724d6ba5a5e2b9b74e01a
Frank Thomsen <frtho@sdfi.dk>
2022-12-13 14:36:40 +0100

    replace switch in getLabel

--- src/modules/type-label.js
+++ src/modules/type-label.js
@@ -1,12 +1,5 @@
 function getLabel(obj) {
-  switch(obj.type) {
-    case 'stednavn':
-      return obj.skrivemaade
-    case 'navngivenvej':
-      return obj.vejnavn
-    default:
-      return obj.adgangsadressebetegnelse
-  }
+  return obj.praesentation || obj.adgangsadressebetegnelse || obj.adressebetegnelse
 }
 
 export {
